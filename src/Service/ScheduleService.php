03c4c44136282fb775d1542d53066c3bf031c74e
Damian Kilian <kilianpoczta@gmail.com>
2024-09-12 18:36:30 +0200

    fix

--- src/Service/ScheduleService.php
+++ src/Service/ScheduleService.php
@@ -17,9 +17,9 @@ class ScheduleService
         $nr = 0;
         $r = $scheduleInquiry->interestRate;
         $k = $yearInstallmentsNum;
-        $subResult1 = bcdiv($r, $k);
-        $subResult2 = bcpow(bcadd('1', $subResult1), $n);
-        $subResult3 = bcdiv(bcmul($subResult1, $subResult2), bcsub($subResult2, '1'));
+        $subResult1 = $r / $k;
+        $subResult2 = pow(1 + $subResult1, $n);
+        $subResult3 = ($subResult1 * $subResult2) / ($subResult2 - 1);
         $installmentAmount = bcmul($scheduleInquiry->amount, $subResult3, 2);
         do {
             $nr++;
